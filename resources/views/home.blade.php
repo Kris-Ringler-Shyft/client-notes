e27e8627f5242ba5c054ac66ea5e6f465aff4217
Kris Ringler <krisringler@kriss-air.attlocal.net>
2020-07-23 17:25:01 -0700

    basic accordian added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,29 @@
 @extends('master')
 
 
-@section('title', 'Homepage')
+@section('title', 'Client Notes')
 
 
 @section('content')
 
-Create Note:
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Example of Creating Bootstrap Collapsible Accordion Widget </title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    Create Note:
 
 <form action="/create" method="post">
     <input type="text" name="title" placeholder="Title">
@@ -15,22 +32,26 @@ Create Note:
     <button type="submit">Create Note</button>
 </form>
 
-Recent Notes:
-
-
-
-<ul>
-    @foreach($notes as $note)
-    <li>
-
-       <strong> {{ $note->title }} </strong>
-        <br/>
-        {{ $note->content }}
-        <br/>
-        {{ $note->created_at->diffForHumans() }}
-        <br/>
-    <a href="/note/{{$note->id}}">View Note</a>
-    </li>
+<div class="panel-group" id="accordion"> <!-- accordion 1 -->
+   @foreach($notes as $note)
+  <div class="panel-group" id="accordion"> <!-- accordion 1 -->
+   <div class="panel panel-primary">
+        <div class="panel-heading"> <!-- panel-heading -->
+            <h4 class="panel-title"> <!-- title 1 -->
+            <a data-toggle="collapse" data-parent="#accordion" href="#accordion{{$note->id}}">
+            {{$note->title}}
+            </a>
+           </h4>
+        </div>
+        <!-- panel body -->
+    <div id="accordion{{$note->id}}" class="panel-collapse collapse in">
+          <div class="panel-body">
+          {{$note->content}} <a href="note/{{$note->id}}" target="_blank">View Note</a>
+          </div>
+        </div>
+  </div>
     @endforeach
-</ul>
+ </div>
+</body>
+</html>
 @endsection
